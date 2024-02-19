155c07307cd82575e551f0e1f24753834d152a17
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-02-20 00:20:34 +0300

    HW41 was fixed

--- app/Http/Controllers/UpdateCustomerProfileController.php
+++ app/Http/Controllers/UpdateCustomerProfileController.php
@@ -12,19 +12,13 @@ class UpdateCustomerProfileController extends BaseController
     public function update(UpdateCustomerProfileRequest $request): JsonResponse
     {
         $request->validated();
-        /*$customer = Customer::where('customerNumber', '=', $request->query('customerNumber'))
+        Customer::where('customerNumber', '=', $request->input('customerNumber'))
             ->update([
-                'contactFirstName', $request->query('contactFirstName'),
-                'contactLastName', $request->query('contactLastName'),
-                'phone', $request->query('phone'),
-                'postalCode', $request->query('postalCode')
-            ]);*/
-        $customer = Customer::find($request->query('customerNumber'));
-        $customer->contactFirstName = $request->query('contactFirstName');
-        $customer->contactLastName = $request->query('contactLastName');
-        $customer->phone = $request->query('phone');
-        $customer->postalCode = $request->query('postalCode');
-        $customer->save();
+                'contactFirstName' => $request->input('contactFirstName'),
+                'contactLastName' => $request->input('contactLastName'),
+                'phone' => $request->input('phone'),
+                'postalCode' => $request->input('postalCode')
+            ]);
         return response()->json(['Успех' => 'Обновление информации о пользователе прошло успешно']);
     }
 }
